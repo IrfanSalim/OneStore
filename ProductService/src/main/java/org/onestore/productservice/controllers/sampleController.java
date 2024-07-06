7b7b964cb7dbbe0aa48dd07533a772e79b2d53e5
Irfan <irfansalim77@gmail.com>
2024-07-06 16:01:03 +0530

    Add ProductService

---
+++ ProductService/src/main/java/org/onestore/productservice/controllers/sampleController.java
@@ -0,0 +1,23 @@
+package org.onestore.productservice.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/say")
+public class sampleController {
+
+    // localhost:8080/say/hello/scaler
+    @GetMapping("/hello/{name}")
+    public String sayHello(@PathVariable("name") String name) {
+        return "Hello " + name;
+    }
+
+    @GetMapping("/bye/{name}")
+    public String sayBye(@PathVariable("name") String name) {
+        return "Bye " + name;
+    }
+
+}
