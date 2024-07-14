988fc5b09fa73b3c821acf6170704e1fa2114745
Irfan <irfansalim77@gmail.com>
2024-07-15 01:19:39 +0530

    Add httpFactory dependency and update RestTemplate

--- ProductService/src/main/java/org/onestore/productservice/configurations/RestTemplateConfig.java
+++ ProductService/src/main/java/org/onestore/productservice/configurations/RestTemplateConfig.java
@@ -2,6 +2,7 @@ package org.onestore.productservice.configurations;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
@@ -9,7 +10,8 @@ public class RestTemplateConfig {
 
     @Bean
     public RestTemplate getRestTemplate() {
-        return new RestTemplate();
+        HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
+        return new RestTemplate(requestFactory);
     }
 
 }
